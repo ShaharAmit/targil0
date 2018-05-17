c3065e532850da93f79dcf2878bcd148797499cf
shahar amit <shaharamit@shahars-mbp.home>
2018-05-17 11:11:10 +0300

    release 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express'),
     app = express(),
     //port = process.env.PORT || 8080;
     //so the call to server will work as expected since it's local server
-    port = 8080,
+    port = process.env.PORT || 8080,
     votesModule = require('./votesModule.js'),
     votes = new votesModule();
     let request='';
@@ -59,5 +59,6 @@ const express = require('express'),
         res.send(messages);
     });
 
-    app.listen(port);
-    console.log(`listening on port ${port}`);
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
